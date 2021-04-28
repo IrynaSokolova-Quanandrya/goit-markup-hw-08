8e1c11337202447964ffc56c051ee7f946a099c7
Iryna <irasokolova25@ukr.net>
2021-04-28 14:47:32 +0300

    Корректирует мобильное меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,9 +6,10 @@
         const expanded =
             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
         
-        menuBtnRef.classList.toggle("mobile__button--is-open");
+        menuBtnRef.classList.toggle("is-open");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-        mobileMenuRef.classList.toggle("mobile__button--is-open");
-    });
+        mobileMenuRef.classList.toggle("is-open");
+
+        });
 })();
